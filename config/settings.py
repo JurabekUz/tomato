45fa0d1064f36ce68c51e64b41b94d8072225403
Jorabek <jurabekdev@gmail.com>
2025-11-08 19:20:58 +0500

    updated db to postgres

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
